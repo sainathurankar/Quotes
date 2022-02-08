5b10e90e34bc36f5bd8d917dee3684bb9390a841
Sainath Urankar <sai.ura999@gmail.com>
2022-02-08 19:06:29 +0530

    adding loader

--- main1.js
+++ main1.js
@@ -5,14 +5,15 @@ var author = "";
 
 
 function change(quote, author) {
+  $("#quoteDisp").css("visibility", "visible");
+  $("dt").css("fontSize", "30px");
+  $("dl").css("fontSize", "15px");
+  $(".loader").css("visibility", "hidden");
   document.getElementById("quote").innerHTML = quote;
   document.getElementById("author").innerHTML = "--" + author;
 }
 
 $(document).ready(function () {
-  $("dt").css("fontSize", "30px");
-  $("dl").css("fontSize", "15px");
-  
 $.get(
   "https://school-buddy-backend-server.herokuapp.com/api/test/qod",
   function (data, status) {
